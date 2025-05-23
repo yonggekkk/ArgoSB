85dde36ea71135daf7f94425369b5e9760c737f4
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-05-23 22:29:51 +0800

    Update argosb_pro.sh

--- argosb_pro.sh
+++ argosb_pro.sh
@@ -218,8 +218,6 @@ fi
 if ( find /proc/*/exe -type l 2>/dev/null | grep -E '/proc/[0-9]+/exe' | xargs -r readlink 2>/dev/null | grep -q 'aspro/s' ) || ( ps aux 2>/dev/null | grep -q '[a]spro/s' ); then
 [ -f ~/.bashrc ] || touch ~/.bashrc
 sed -i '/yonggekkk/d' ~/.bashrc
-#echo "export ip=${ipsw} argo=${argo} uuid=${uuid} vlpt=${port_vl_re} vmpt=${port_vm_ws} hypt=${port_hy2} tupt=${port_tu} reym=${ym_vl_re} agn=${ARGO_DOMAIN} agk=${ARGO_AUTH} && bash <(curl -Ls https://raw.githubusercontent.com/yonggekkk/argosb/beta/argosb_pro.sh)" >> ~/.bashrc
-#echo "sbpid=\$(cat ./aspro/sbpid.log 2>/dev/null); sbpidp=\$(cat /proc/\$sbpid/status 2>/dev/null); if [ -z \"\$sbpidp\" ] && ! ps -p \"\$sbpid\" > /dev/null 2>&1; then export ip=\"${ipsw}\" argo=\"${argo}\" uuid=\"${uuid}\" vlpt=\"${port_vl_re}\" vmpt=\"${port_vm_ws}\" hypt=\"${port_hy2}\" tupt=\"${port_tu}\" reym=\"${ym_vl_re}\" agn=\"${ARGO_DOMAIN}\" agk=\"${ARGO_AUTH}\"; bash <(curl -Ls https://raw.githubusercontent.com/yonggekkk/argosb/beta/argosb_pro.sh); fi" >> ~/.bashrc
 echo "if ! ( find /proc/*/exe -type l 2>/dev/null | grep -E '/proc/[0-9]+/exe' | xargs -r readlink 2>/dev/null | grep -q 'aspro/s' ) && ! ( ps aux 2>/dev/null | grep -q '[a]spro/s' ); then export ip=\"${ipsw}\" argo=\"${argo}\" uuid=\"${uuid}\" vlpt=\"${port_vl_re}\" vmpt=\"${port_vm_ws}\" hypt=\"${port_hy2}\" tupt=\"${port_tu}\" reym=\"${ym_vl_re}\" agn=\"${ARGO_DOMAIN}\" agk=\"${ARGO_AUTH}\"; bash <(curl -Ls https://raw.githubusercontent.com/yonggekkk/argosb/beta/argosb_pro.sh); fi" >> ~/.bashrc
 COMMAND="asp"
 SCRIPT_PATH="$HOME/bin/$COMMAND"
