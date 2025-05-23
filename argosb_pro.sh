9d493dd778ae16c251a00352989a48fdc5ac80cc
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-05-23 11:37:35 +0800

    Update argosb_pro.sh

--- argosb_pro.sh
+++ argosb_pro.sh
@@ -382,7 +382,7 @@ kill -15 $(cat ./aspro/sbpid.log 2>/dev/null) >/dev/null 2>&1
 sed -i '/yonggekkk/d' ~/.bashrc
 sed -i '/export PATH="\$HOME\/bin:\$PATH"/d' ~/.bashrc
 source ~/.bashrc
-rm -rf ./aspro /bin/asp
+rm -rf ./aspro ./bin/asp
 echo "卸载完成"
 exit
 elif [[ "$1" == "list" ]]; then
