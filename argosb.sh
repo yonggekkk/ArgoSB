f7e2aab9545316352853580591d220d8ac62e361
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-05-24 12:07:35 +0800

    Update argosb.sh

--- argosb.sh
+++ argosb.sh
@@ -180,7 +180,7 @@ if [[ -n "${ARGO_DOMAIN}" && -n "${ARGO_AUTH}" ]]; then
 echo '@reboot /bin/bash -c "nohup ./as/cloudflared tunnel --no-autoupdate --edge-ip-version auto --protocol http2 run --token $(cat ./as/sbargotoken.log 2>/dev/null) >/dev/null 2>&1 &"' >> /tmp/crontab.tmp
 else
 #echo '@reboot /bin/bash -c "nohup ./as/cloudflared tunnel --url http://localhost:$(grep "listen_port" ./as/sb.json | grep -oP '\''\d+'\'' | sed -n '\''1p'\'') --edge-ip-version auto --no-autoupdate --protocol http2 > ./as/argo.log 2>&1 &"' >> /tmp/crontab.tmp
-echo '@reboot /bin/bash -c "sleep 10 && PORT=$(grep -oP '\"listen_port\":\s*\K\d+' ./as/sb.json | head -n 1) && nohup ./as/cloudflared tunnel --url http://localhost:$PORT --edge-ip-version auto --no-autoupdate --protocol http2 > ./as/argo.log 2>&1 &"' >> /tmp/crontab.tmp
+echo '@reboot /bin/bash -c "sleep 10 && PORT=$(grep "listen_port" ./as/sb.json | grep -oP '\''\d+'\'' | sed -n '\''1p'\'') && nohup ./as/cloudflared tunnel --url http://localhost:$PORT --edge-ip-version auto --no-autoupdate --protocol http2 > ./as/argo.log 2>&1 &"' >> /tmp/crontab.tmp
 
 fi
 crontab /tmp/crontab.tmp 2>/dev/null
