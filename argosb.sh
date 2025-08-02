5ceba1362e7a6fc8563d11438628403ce7231812
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-08-02 17:21:16 +0800

    Update argosb.sh

--- argosb.sh
+++ argosb.sh
@@ -128,6 +128,11 @@ cat > "$HOME/agsb/xr.json" <<EOF
     "error": "/dev/null",
     "loglevel": "none"
   },
+"dns": {
+  "servers": [
+    "https+local://8.8.8.8/dns-query"
+  ]
+},
   "inbounds": [
 EOF
 insuuid
@@ -495,7 +500,7 @@ cat >> "$HOME/agsb/xr.json" <<EOF
     }
   ],
   "routing": {
-  "domainStrategy": "IPIfNonMatch",
+  "domainStrategy": "IPOnDemand",
     "rules": [
       {
         "type": "field",
