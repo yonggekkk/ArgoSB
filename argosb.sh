f1bba25ec74206b98d0036350688aa12a6e28dc2
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-08-24 21:14:15 +0800

    Update argosb.sh

--- argosb.sh
+++ argosb.sh
@@ -98,7 +98,8 @@ fi
 fi
 case "$warp" in x4) wxryx='ForceIPv4' ;; x6) wxryx='ForceIPv6' ;; *) wxryx='ForceIPv4v6' ;; esac
 case "$warp" in x4) xryx=$(curl -s6m5 icanhazip.com -k >/dev/null && echo ForceIP || echo ForceIPv4) ;; *) xryx=ForceIP ;; esac
-sbyx=$(curl -s4m5 icanhazip.com -k >/dev/null && echo prefer_ipv6 || echo prefer_ipv4)
+#sbyx=$(curl -s4m5 icanhazip.com -k >/dev/null && echo prefer_ipv6 || echo prefer_ipv4)
+sbyx=prefer_ipv4
 }
 
 insuuid(){
