31d74ca829fea9ae8165bd3870987c6317220c91
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-09-01 17:10:55 +0800

    Update argosb.sh

--- argosb.sh
+++ argosb.sh
@@ -104,8 +104,16 @@ esac
 fi
 fi
 case "$warp" in *x4*) wxryx='ForceIPv4' ;; *x6*) wxryx='ForceIPv6' ;; *) wxryx='ForceIPv4v6' ;; esac
-case "$warp" in *x4*|*s4*) if command -v curl >/dev/null 2>&1 && curl -s6m5 -k "$v46url" >/dev/null || command -v wget >/dev/null 2>&1 && wget -6 --tries=2 -qO- "$v46url" >/dev/null; then xryx='ForceIPv6v4' sbyx='prefer_ipv6'; else xryx='ForceIPv4' sbyx='ipv4_only'; fi ;; *) xryx='ForceIPv6v4' sbyx='prefer_ipv6' ;; esac
-case "$warp" in *x6*|*s6*) if command -v curl >/dev/null 2>&1 && curl -s6m5 -k "$v46url" >/dev/null || command -v wget >/dev/null 2>&1 && wget -6 --tries=2 -qO- "$v46url" >/dev/null; then xryx='ForceIPv4v6' sbyx='prefer_ipv4'; else xryx='ForceIPv6v4' sbyx='prefer_ipv6'; fi ;; *) xryx='ForceIPv6v4' sbyx='prefer_ipv6' ;; esac
+if command -v curl >/dev/null 2>&1 && curl -s6m5 -k "$v46url" >/dev/null || command -v wget >/dev/null 2>&1 && wget -6 --tries=2 -qO- "$v46url" >/dev/null; then
+xryx='ForceIPv6v4'; sbyx='prefer_ipv6'
+else
+case "$warp" in *x4*) xryx='ForceIPv4' ;; esac
+case "$warp" in *x6*) xryx='ForceIPv6v4' ;; esac
+case "$warp" in *s4*) sbyx='ipv4_only' ;; esac
+case "$warp" in *s6*) sbyx='prefer_ipv6' ;; esac
+[ -z "$xryx" ] && xryx='ForceIPv6v4'
+[ -z "$sbyx" ] && sbyx='prefer_ipv6'
+fi
 }
 
 insuuid(){
