86832892967c1523d19e9602ad175c049b099b2c
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-10-11 08:39:54 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,8 @@
 export default {
   async fetch(request, env) {
-    const targetHost='域名'
-    let url = new URL(request.url)
-    if (url.pathname.startsWith('/')) {
-      url.hostname = targetHost
-      let new_request = new Request(url, request)
-      return fetch(new_request)
-    }
-    return env.ASSETS.fetch(request)
+    const url = new URL(request.url)
+    url.protocol = 'https:'
+    url.hostname = '域名'
+    return fetch(new Request(url, request))
   }
 }
